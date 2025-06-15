31af4183912422e2b28e4158ef4b3b763a8fa2c3
UllasDas004 <ullasdas110904@gmail.com>
2025-06-15 10:05:34 +0530

    Q 5 Jagged Swaps solve from some video help

---
+++ 800/05_JaggedSwaps.cpp
@@ -0,0 +1,21 @@
+// Q = 1896A
+// https://codeforces.com/problemset/problem/1896/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        bool isSorted = true;
+        for(int i=0;i<n;i++) cin>>a[i];
+        if(a[0] == 1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}

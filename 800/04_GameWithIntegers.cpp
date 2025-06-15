dc75de298e62409e71cacb5a292caf1c00fb1a9a
UllasDas004 <ullasdas110904@gmail.com>
2025-06-15 09:24:58 +0530

    Q 4 Game With Integers solved

---
+++ 800/04_GameWithIntegers.cpp
@@ -0,0 +1,18 @@
+// Q = 1899A
+// https://codeforces.com/problemset/problem/1899/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int  main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n % 3 != 0) cout<<"First"<<endl;
+        else cout<<"Second"<<endl;
+    }
+    return 0;
+}

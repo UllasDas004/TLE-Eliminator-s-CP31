85cccf80f843052145f1795f9bd28dbdc5360144
UllasDas004 <ullasdas110904@gmail.com>
2025-06-15 19:24:23 +0530

    Q08 How Much Does Daytona Cost? solved

--- 800/08_HowMuchDoesDaytonaCost.cpp
+++ 800/08_HowMuchDoesDaytonaCost.cpp
@@ -5,5 +5,21 @@
 using namespace std;
 int main()
 {
-    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int> a(n);
+        bool flag = false;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            if(a[i] == k) flag = true;
+        }
+        if(flag) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
 }

89cba65fbe2b710412432a60cb44380014902baf
UllasDas004 <ullasdas110904@gmail.com>
2025-12-03 13:01:36 +0530

    Q21 Pleasant Pairs solved with help

---
+++ 1200/21_PleasantPairs.cpp
@@ -0,0 +1,36 @@
+// Q = 1541/B
+// https://codeforces.com/problemset/problem/1541/B
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vector<pair<ll,int>> a(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i].first;
+            a[i].second = i+1;
+        }
+        sort(a.begin(),a.end());
+        ll count = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                ll product = a[i].first * a[j].first;
+                if(product >= 2*n) break;
+                ll indexSum = a[i].second + a[j].second;
+                if(product == indexSum) count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
